6b04e60748ff71d42c22daf19717eca4a00984c4
roeihaviv <rhaviv@chicagobooth.edu>
2023-01-24 21:56:16 +0000

    Edit works

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -33,6 +33,10 @@ class TasksController < ApplicationController
 
   # GET /tasks/1/edit
   def edit
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   # POST /tasks or /tasks.json
@@ -56,9 +60,11 @@ class TasksController < ApplicationController
       if @task.update(task_params)
         format.html { redirect_to task_url(@task), notice: "Task was successfully updated." }
         format.json { render :show, status: :ok, location: @task }
+        format.js
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @task.errors, status: :unprocessable_entity }
+        format.js
       end
     end
   end
