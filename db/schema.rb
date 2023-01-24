8d06a1e87509bf3d7da688b6bb6871e801a20e05
roeihaviv <rhaviv@chicagobooth.edu>
2023-01-24 21:49:45 +0000

    Added user_id column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_24_204458) do
+ActiveRecord::Schema.define(version: 2023_01_24_214909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2023_01_24_204458) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "status"
     t.text "body"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -7,6 +7,7 @@
 #  status     :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :integer
 #
 require "application_system_test_case"
 
